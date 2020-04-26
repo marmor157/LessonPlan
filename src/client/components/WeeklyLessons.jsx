bfcc2648ed38c4d3618638ba47e7c3118d1d5364
marmor157 <marmor19@gmail.com>
2020-04-26 18:43:38 +0200

    Exported hours sidetable to own component

--- src/client/components/WeeklyLessons.jsx
+++ src/client/components/WeeklyLessons.jsx
@@ -1,28 +1,12 @@
 import React, { PureComponent } from "react";
 import WeeklyLessonsElement from "./WeeklyLessonsElement";
-import formatHour from "../utils/formatHour";
-import minutesToHour from "../utils/minutesToHour";
+
 import getNameDay from "../utils/getDayName";
+import TableWithHours from "./TableWithHours";
 
 export default class WeeklyLessons extends PureComponent {
   render() {
     const { lessons, hours } = this.props;
-
-    //Map user's hours
-    const hoursDisplay = hours.map((hour, id) => {
-      const { hour: startHour, minute: startMinute } = formatHour(
-        minutesToHour(hour.startHour)
-      );
-      const { hour: finishHour, minute: finishMinute } = formatHour(
-        minutesToHour(hour.finishHour)
-      );
-
-      return (
-        <div className="weeklyLessons__element weeklyLessons__element--hour">
-          {startHour}:{startMinute} - {finishHour}:{finishMinute}
-        </div>
-      );
-    });
 
     //Map user's lessons with day name as a top element
     const lessonsDisplay = lessons.map((lessonRow, id) => (
@@ -35,14 +19,11 @@ export default class WeeklyLessons extends PureComponent {
       </div>
     ));
 
+    const hourWithEmptySlot = [{}, ...hours];
     return (
-      <div className="weeklyLessons">
-        <div className="weeklyLessons__hourColumn">
-          <div className="weeklyLessons__element weeklyLessons__element--hour"></div>
-          {hoursDisplay}
-        </div>
-        <div className="weeklyLessons__lessonsContainer">{lessonsDisplay}</div>
-      </div>
+      <TableWithHours hours={hourWithEmptySlot}>
+        <div className="weeklyLessons">{lessonsDisplay}</div>
+      </TableWithHours>
     );
   }
 }
